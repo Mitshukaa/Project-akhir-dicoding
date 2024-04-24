f0c6fe527c16f9d580d251e447e01b2bad65bcec
Mitshukaa <ferdinandviano@gmail.com>
2024-04-24 21:35:38 +0700

    Berhasil Menyelesaikan Portofolio simple

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     document.querySelector('.navbar').classList.toggle('active');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    var htmlProgress = document.getElementById("html-progress");
+    
+    var htmlProgressLabel = document.getElementById("html-progress-label");
+    
+
+    htmlProgressLabel.innerHTML = htmlProgress.value + "%";
+
+});
